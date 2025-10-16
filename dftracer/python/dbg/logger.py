a8c59987dc0ee7b3d8551b6436eacbe0813c11e2
Ray Andrew <rs@rs.ht>
2025-10-16 01:52:38 -0500

    add logging info when dftracer is enabled

--- dftracer/python/dbg/logger.py
+++ dftracer/python/dbg/logger.py
@@ -1,11 +1,14 @@
 from dftracer.python.env import *
 
 logger = setup_stream_logger()
-import dftracer.python.common
+
+import dftracer.python.common as common
+
 if DFTRACER_ENABLE:
     try:
         import dftracer.dftracer_dbg as profiler
-        dftracer.python.common.profiler = profiler
+        common.profiler = profiler
+        logger.info("DFTracer was enabled.")
     except ImportError:
         logger.info("DFTracer was enabled but not available.")
         pass
